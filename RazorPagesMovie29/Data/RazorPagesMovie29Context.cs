1627ff1dda0fce3bbd58594990e12e3344457d22
Scot <bscot.hagen@gmail.com>
2021-08-12 13:08:24 -0700

    Added Search

---
+++ RazorPagesMovie29/Data/RazorPagesMovie29Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie29.Models;
+
+namespace RazorPagesMovie29.Data
+{
+    public class RazorPagesMovie29Context : DbContext
+    {
+        public RazorPagesMovie29Context (DbContextOptions<RazorPagesMovie29Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesMovie29.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ RazorPagesMovie29/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RazorPagesMovie29.Data;
+using RazorPagesMovie29.Models;
+
+namespace RazorPagesMovie29.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly RazorPagesMovie29.Data.RazorPagesMovie29Context _context;
+
+        public CreateModel(RazorPagesMovie29.Data.RazorPagesMovie29Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
